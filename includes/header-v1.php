330967b3491ce388b0d91922da01e87f58ebba5e
nadia-rais <nadia.rais@laplateforme.io>
2020-07-15 08:52:20 +0200

    index
    1ère version index + ajout autre header

---
+++ includes/header-v1.php
@@ -0,0 +1,61 @@
+<?php 
+if (isset($_POST["deco"])) {
+    session_unset();
+    session_destroy();
+    header('Location:index.php');
+}
+?>
+<header>
+  <section id="top-header1">
+    <a href="newsletter-form.php">RECEVOIR NOTRE BROCHURE</a>
+    <a id="header-title" href="reservation-form.php">RESERVER</a>
+  </section>
+  <section>
+    <nav>
+      <ul id="nav_links1">
+        <li><a href="planning.php">NOS DISPONIBILITÉS</a></li>
+        <li><a href="#">NOS SERVICES</a></li>
+        <li id="title">
+          <a href="index.php">
+            <img id="logo1" src="https://i.ibb.co/hMhFxXF/logotype1.png" alt="logotype1">
+            <h1>camping boheme chic</h1>
+          </a>
+        </li>
+        <?php 
+        if (isset($_SESSION["login"])){
+          if($_SESSION["login"] == "admin"){
+          ?>
+          <li><a href="admin.php">PANEL BOARD</a></li>
+          
+            <form  action="index.php" method="post">
+              <input id="deco" name="deco" value="DECONNEXION" type="submit"/>
+            </form>
+       
+          <?php 
+          }
+          else{
+          ?>
+
+          <li><a href="profil.php">MON COMPTE</a></li>
+        
+          <form  action="index.php" method="post">
+            <input id="deco" name="deco" value="DECONNEXION" type="submit"/>
+          </form>
+       
+
+      <?php
+          }
+      ?> 
+     
+      <?php
+        }else{
+      ?>
+      <li><a href="contact-form.php">NOUS CONTACTER</a></li>
+      <li><a id="deco" href="connexion.php"> SE CONNECTER</a></li>
+      </ul>
+
+      <?php }?>
+
+    </nav>
+  </section>
+</header>
